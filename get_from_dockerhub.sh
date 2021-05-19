1522efb5286b2598a881088aea12c9bb5f2f8735
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2021-05-19 13:26:50 +0100

    Update get_from_dockerhub.sh

--- get_from_dockerhub.sh
+++ get_from_dockerhub.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash                                                                                                                         
                                                                                                                                             
-mkdir -p cache_dir                                                                                                                          
-export SINGULARITY_CACHEDIR=$PWD/cache_dir                                                                                                  
-                                                                                                                                            
-mkdir -p tmp_dir                                                                                                                            
-export TMPDIR=$PWD/tmp_dir                                                                                                                  
-                                                                                                                                            
-# see https://github.com/wtsi-hgi/scrna_deconvolution_container                                                                             
-rm -f scrna_deconvolution_latest.sif                                                                                                        
-singularity pull docker://mercury/scrna_deconvolution      
+# this script is placed/run at /lustre/scratch123/hgi/projects/ukbb_scrna/pipelines/singularity_images/ 
 
-rm -r cache_dir
-rm -r tmp_dir
+# choose dockerhub image tag
+# cf. https://github.com/wtsi-hgi/scrna_deconvolution_container
+tag=1.0
+
+mkdir -p cache_dir
+export SINGULARITY_CACHEDIR=$PWD/cache_dir
+
+mkdir -p tmp_dir
+export TMPDIR=$PWD/tmp_dir
+
+rm -f scrna_deconvolution_latest.sif || true
+singularity pull docker://mercury/scrna_deconvolution:${tag}
+
+# place as 'latest' tag for FCE scrna deconvolution pipeline to pick up.
+# see https://gitlab.internal.sanger.ac.uk/gn5/single-cell-nextflow-ci/-/blob/deconv_nogenotype_sbw_Deconvolution_Exp4/wr_cloud_script.sh
+ln -s scrna_deconvolution_${tag}.sif scrna_deconvolution_latest.sif
